7938de881d96ebf053d2c6763601bc45aeff2338
prokazin <nazar.bronnikov22@gmail.com>
2026-07-17 08:38:40 +0900

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#8B5CF6',
+        background: '#181818',
+        card: '#2A2A2A',
+      },
+    },
+  },
+  plugins: [],
+}
