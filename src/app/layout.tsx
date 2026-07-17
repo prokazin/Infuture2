0899fcb2a3353cf63252e6f5c2d1c20b2332ee88
prokazin <nazar.bronnikov22@gmail.com>
2026-07-17 11:20:06 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <head>
+        {/* Telegram WebApp */}
         <script src="https://telegram.org/js/telegram-web-app.js" />
+        {/* Font Awesome для иконок */}
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
       </head>
       <body>{children}</body>
