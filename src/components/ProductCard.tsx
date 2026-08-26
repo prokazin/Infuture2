4a192401b59d60ee0b18235560761f75e49b1d85
prokazin <nazar.bronnikov22@gmail.com>
2026-08-26 20:51:35 +0900

    Update ProductCard.tsx

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -25,31 +25,45 @@ export default function ProductCard({ product, onAddToCart }: Props) {
     return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '') + 'р'
   }
 
+  // Определяем имя файла изображения из Figma
+  const getImagePath = (product: Product) => {
+    const imageMap: Record<string, string> = {
+      'iPhone 17 pro max': '/img/iphone-1.png',
+      'Samsung S26': '/img/samsung-s26.png',
+    }
+    return imageMap[product.name] || product.images[0]
+  }
+
   return (
     <Link href={`/product/${product.id}`}>
-      <div className="product-card cursor-pointer">
-        {/* Изображение - как в Figma */}
-        <div className="product-image relative">
+      <div className="product-card-figma cursor-pointer">
+        {/* Изображение товара - orig-3-no-bg-preview-carve-photos-1 */}
+        <div className="absolute top-[-16px] left-1/2 -translate-x-1/2 w-[144px] h-[199px]">
           <Image
-            src={product.images[0]}
+            src={getImagePath(product)}
             alt={product.name}
             width={144}
             height={199}
             className="object-contain"
           />
         </div>
-        
-        {/* Информация о товаре - как в Figma */}
-        <div className="product-info">
-          <div className="product-name">{product.name}</div>
-          <div className="product-memory">{product.memory}</div>
-          <div className="product-price">{formatPrice(product.price)}</div>
-          <button
+
+        {/* Разделительная линия - line-4 */}
+        <div className="line-4"></div>
+
+        {/* Информация о товаре - frame-122 */}
+        <div className="frame-122">
+          <div className="i-phone-17-pro-max">{product.name}</div>
+          <div className="_256-gb">{product.memory}</div>
+          <div className="_117-000">{formatPrice(product.price)}</div>
+          <div 
+            className="rectangle-995"
             onClick={handleAdd}
-            className="btn-cart-figma"
           >
-            {added ? '✓' : 'в корзину'}
-          </button>
+            <span className="btn-cart-text">
+              {added ? '✓' : 'в корзину'}
+            </span>
+          </div>
         </div>
       </div>
     </Link>
